ff6862a697c03e3b86e0b9e7bac4ba21a6d5b943
harshgajera101 <xsport102@gmail.com>
2024-10-06 14:10:28 +0530

    new frontend only

--- src/routes/itinerary.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { fetchDataItinerary } from "../controllers/itinerary.controller.js";
-
-const router = Router();
-
-router.route('/itinerary').post(fetchDataItinerary)
-
-export default router
